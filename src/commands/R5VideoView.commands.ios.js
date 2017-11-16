58c4f499b13412d44d70968aaeeeafcecbba317b
bustardcelly <bustardcelly@gmail.com>
2017-11-16 14:38:37 -0500

    passing scale in ios command.

--- src/commands/R5VideoView.commands.ios.js
+++ src/commands/R5VideoView.commands.ios.js
@@ -7,5 +7,5 @@ export const unsubscribe = (handle) => R5VideoViewManager.unsubscribe()
 export const publish = (handle, streamName, streamType = R5PublishType.LIVE) => R5VideoViewManager.publish(streamName, streamType)
 export const unpublish = (handle) => R5VideoViewManager.unpublish()
 export const swapCamera = (handle) => R5VideoViewManager.swapCamera()
-export const updateScaleMode = (handle) => R5VideoViewManager.updateScaleMode()
+export const updateScaleMode = (handle, scale) => R5VideoViewManager.updateScaleMode(scale)
 
