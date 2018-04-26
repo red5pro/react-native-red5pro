3ad296fb0744a2c7ff2b70e73fc9a3137e07fe73
bustardcelly <bustardcelly@gmail.com>
2018-04-26 14:04:48 -0400

    separating concerns for publisher and subscriber in examples. #11

--- example/App.js
+++ example/App.js
@@ -1,37 +1,27 @@
 import React from 'react'
-import { findNodeHandle, Button, StyleSheet, Text, TextInput, View } from 'react-native'
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View
+} from 'react-native'
 import Permissions from 'react-native-permissions'
-import { R5VideoView } from 'react-native-red5pro'
-import { R5ScaleMode, R5LogLevel } from 'react-native-red5pro'
-import { subscribe,
-         unsubscribe,
-         publish,
-         unpublish,
-         swapCamera,
-         updateScaleMode } from 'react-native-red5pro'
+import { 
+  R5LogLevel,
+} from 'react-native-red5pro'
 
-const isValidStatusMessage = (value) => {
-  return value && typeof value !== 'undefined' && value !== 'undefined' && value !== 'null'
-}
+import Publisher from './src/views/publisher'
+import Subscriber from './src/views/subscriber'
 
 export default class App extends React.Component {
   constructor (props) {
     super(props)
 
-    // Events.
-    this.onMetaData = this.onMetaData.bind(this)
-    this.onConfigured = this.onConfigured.bind(this)
-    this.onPublisherStreamStatus = this.onPublisherStreamStatus.bind(this)
-    this.onSubscriberStreamStatus = this.onSubscriberStreamStatus.bind(this)
-    this.onUnsubscribeNotification = this.onUnsubscribeNotification.bind(this)
-    this.onUnpublishNotification = this.onUnpublishNotification.bind(this)
-
     // Actions.
     this.onPublish = this.onPublish.bind(this)
     this.onSubscribe = this.onSubscribe.bind(this)
     this.onStop = this.onStop.bind(this)
-    this.onSwapCamera = this.onSwapCamera.bind(this)
-    this.onScaleMode = this.onScaleMode.bind(this)
 
     // UI Actions.
     this.onHostChange = this.onHostChange.bind(this)
@@ -44,44 +34,36 @@ export default class App extends React.Component {
       hasStarted: false,
       isPublisher: false,
       isInErrorState: false,
-      buttonProps: {
-        style: styles.buttonView
-      },
       hostFieldProps: {
         placeholder: 'Host',
         autoCorrect: false,
         underlineColorAndroid: '#00000000',
         clearTextOnFocus: true,
-        style: styles.inputField
+        style: styles.inputField,
+        value: ''
       },
       licenseFieldProps: {
         placeholder: 'License Key',
         autoCorrect: false,
         underlineColorAndroid: '#00000000',
         clearTextOnFocus: true,
-        style: styles.inputField
+        style: styles.inputField,
+        value: ''
       },
       streamNameFieldProps: {
         placeholder: 'Stream Name',
         autoCorrect: false,
         underlineColorAndroid: '#00000000',
         clearTextOnFocus: true,
-        style: styles.inputField
+        style: styles.inputField,
+        value: ''
       },
-      toastProps: {
-        ref: 'toast',
-        style: styles.toast,
-        value: 'waiting...'
-      },
-      videoProps: {
-        ref: 'video',
-        key: 'video',
-        style: styles.videoView,
+      streamProps: {
         collapsable: false,
         configuration: {
-          host: "",
-          licenseKey: "",
-          streamName: "",
+          host: '',
+          licenseKey: '',
+          streamName: '',
           port: 8554,
           contextName: 'live',
           bufferTime: 0.5,
@@ -91,19 +73,10 @@ export default class App extends React.Component {
         },
         showDebugView: true,
         logLevel: R5LogLevel.DEBUG,
-        scaleMode: R5ScaleMode.SCALE_TO_FILL,
-        useBackfacingCamera: false,
-        onMetaData: this.onMetaData,
-        onConfigured: this.onConfigured,
-        onPublisherStreamStatus: this.onPublisherStreamStatus,
-        onSubscriberStreamStatus: this.onSubscriberStreamStatus,
-        onUnsubscribeNotification: this.onUnsubscribeNotification,
-        onUnpublishNotification: this.onUnpublishNotification
+        useBackfacingCamera: false
       }
     }
 
-    // stored for scale swapping. #see :onSwapScale
-    this.scaleMode = this.state.videoProps.scaleMode
   }
 
   componentDidMount () {
@@ -123,50 +96,27 @@ export default class App extends React.Component {
   }
 
   render () {
-    const assignVideoRef = (video) => { this.red5pro_video = video }
+    const assignHostRef = (host) => { this.host_field = host }
+    const assignLicenseRef = (license) => { this.license_field = license }
+    const assignStreamNameRef = (streamName) => { this.stream_name_field = streamName }
+
     if (this.state.hasPermissions && this.state.hasStarted) {
       if (this.state.isPublisher) {
         return (
-          <View style={styles.container}>
-            <R5VideoView {...this.state.videoProps}
-              ref={assignVideoRef.bind(this)}
-            />
-            <Text {...this.state.toastProps}>{this.state.toastProps.value}</Text>
-            <Button
-              {...this.state.buttonProps}
-              onPress={this.onStop}
-              title='Stop'
-              accessibilityLabel='Stop'
-              />
-            <Button
-              {...this.state.buttonProps}
-              onPress={this.onSwapCamera}
-              title='Swap Camera'
-              accessibilityLabel='Swap Camera'
-              />
-          </View>
+          <Publisher
+            streamProps={this.state.streamProps}
+            style={styles.container}
+            onStop={this.onStop}
+          />
         )
       }
       else {
         return (
-          <View style={styles.container}>
-            <R5VideoView {...this.state.videoProps}
-              ref={assignVideoRef.bind(this)}
-            />
-            <Text {...this.state.toastProps}>{this.state.toastProps.value}</Text>
-            <Button
-              {...this.state.buttonProps}
-              onPress={this.onStop}
-              title='Stop'
-              accessibilityLabel='Stop'
-              />
-            <Button
-              {...this.state.buttonProps}
-              onPress={this.onScaleMode}
-              title='Swap Scale'
-              accessibilityLabel='Swap Scale'
-              />
-          </View>
+          <Subscriber
+            streamProps={this.state.streamProps}
+            style={styles.container}
+            onStop={this.onStop}
+          />
         )
       }
     }
@@ -174,39 +124,44 @@ export default class App extends React.Component {
       return (
         <View style={styles.container}>
           <View style={styles.formField}>
-            <TextInput ref="host"
+            <TextInput
+              ref={assignHostRef.bind(this)}
               {...this.state.hostFieldProps}
               onChangeText={this.onHostChange}
             />
           </View>
           <View style={styles.formField}>
-            <TextInput ref="license"
+            <TextInput
+              ref={assignLicenseRef.bind(this)}
               {...this.state.licenseFieldProps}
               onChangeText={this.onLicenseChange}
             />
           </View>
           <View style={styles.formField}>
-            <TextInput ref="streamName"
+            <TextInput
+              ref={assignStreamNameRef.bind(this)}
               {...this.state.streamNameFieldProps}
               onChangeText={this.onStreamNameChange}
             />
           </View>
-          {!this.state.hasPermissions
-            ? <Text style={{color: 'white', backgroundColor: 'blue'}}>Waiting on permissions...</Text>
-            : <View><Button
-                {...this.state.buttonProps}
-                onPress={this.onSubscribe}
-                title='Subscribe'
-                accessibilityLabel='Subscribe'
-                />
-              <Text style={styles.text}>OR</Text>
-              <Button
-                {...this.state.buttonProps}
-                style={{marginTop: 40}}
-                onPress={this.onPublish}
-                title='Publish'
-                accessibilityLabel='Publish'
-                /></View>}
+          {!this.state.hasPermissions && <Text style={{color: 'white', backgroundColor: 'blue'}}>Waiting on permissions...</Text>}
+          {this.state.hasPermissions && <View>
+            <Button
+              {...this.state.buttonProps}
+              onPress={this.onSubscribe}
+              title='Subscribe'
+              accessibilityLabel='Subscribe'
+            />
+            <Text style={styles.text}>OR</Text>
+            <Button
+              {...this.state.buttonProps}
+              style={{marginTop: 40}}
+              onPress={this.onPublish}
+              title='Publish'
+              accessibilityLabel='Publish'
+            />
+          </View>
+          }
         </View>
       )
     }
@@ -231,15 +186,18 @@ export default class App extends React.Component {
   }
 
   onSubscribe (event) {
+    const hostValue = this.host_field.props.value
+    const licenseValue = this.license_field.props.value
+    const streamNameValue = this.stream_name_field.props.value
     this.setState({
-      hostFieldProps: {...this.state.hostFieldProps, value: this.refs.host.props.value},
-      licenseFieldProps: {...this.state.licenseFieldProps, value: this.refs.license.props.value},
-      streamNameFieldProps: {...this.state.streamNameFieldProps, value: this.refs.streamName.props.value},
-      videoProps: {...this.state.videoProps,
-        configuration: {...this.state.videoProps.configuration,
-          host: this.refs.host.props.value,
-          licenseKey: this.refs.license.props.value,
-          streamName: this.refs.streamName.props.value
+      hostFieldProps: {...this.state.hostFieldProps, value: hostValue},
+      licenseFieldProps: {...this.state.licenseFieldProps, value: licenseValue},
+      streamNameFieldProps: {...this.state.streamNameFieldProps, value: streamNameValue},
+      streamProps: {...this.state.streamProps,
+        configuration: {...this.state.streamProps.configuration,
+          host: hostValue,
+          licenseKey: licenseValue,
+          streamName: streamNameValue
         }
       },
       isPublisher: false,
@@ -248,15 +206,18 @@ export default class App extends React.Component {
   }
 
   onPublish (event) {
+    const hostValue = this.host_field.props.value
+    const licenseValue = this.license_field.props.value
+    const streamNameValue = this.stream_name_field.props.value
     this.setState({
-      hostFieldProps: {...this.state.hostFieldProps, value: this.refs.host.props.value},
-      licenseFieldProps: {...this.state.licenseFieldProps, value: this.refs.license.props.value},
-      streamNameFieldProps: {...this.state.streamNameFieldProps, value: this.refs.streamName.props.value},
-      videoProps: {...this.state.videoProps,
-        configuration: {...this.state.videoProps.configuration,
-          host: this.refs.host.props.value,
-          licenseKey: this.refs.license.props.value,
-          streamName: this.refs.streamName.props.value
+      hostFieldProps: {...this.state.hostFieldProps, value: hostValue},
+      licenseFieldProps: {...this.state.licenseFieldProps, value: licenseValue},
+      streamNameFieldProps: {...this.state.streamNameFieldProps, value: streamNameValue},
+      streamProps: {...this.state.streamProps,
+        configuration: {...this.state.streamProps.configuration,
+          host: hostValue,
+          licenseKey: licenseValue,
+          streamName: streamNameValue
         }
       },
       isPublisher: true,
@@ -264,38 +225,11 @@ export default class App extends React.Component {
     })
   }
 
-  onStop (event) {
-    console.log('onStop()')
-    if (this.state.isPublisher) {
-      unpublish(findNodeHandle(this.red5pro_video))
-    }
-    else {
-      unsubscribe(findNodeHandle(this.red5pro_video))
-    }
-  }
-
-  onSwapCamera (event) {
-    console.log('onSwapCamera()')
-    if (this.state.isPublisher) {
-      swapCamera(findNodeHandle(this.red5pro_video))
-    }
-  }
-
-  onScaleMode (event) {
-    console.log('onScaleMode()')
-    let scale = this.scaleMode + 1
-    if (scale > 2) {
-      scale = 0
-    }
-    this.scaleMode = scale
-    updateScaleMode(findNodeHandle(this.red5pro_video), scale)
-  }
-
   onHostChange (text) {
     this.setState({
       hostFieldProps: {...this.state.hostFieldProps, value: text},
-      videoProps: {...this.state.videoProps,
-        configuration: {...this.state.videoProps.configuration, host: text}
+      streamProps: {...this.state.streamProps,
+        configuration: {...this.state.streamProps.configuration, host: text}
       }
     })
   }
@@ -303,8 +237,8 @@ export default class App extends React.Component {
   onLicenseChange (text) {
     this.setState({
       licenseFieldProps: {...this.state.licenseFieldProps, value: text},
-      videoProps: {...this.state.videoProps,
-        configuration: {...this.state.videoProps.configuration, licenseKey: text}
+      streamProps: {...this.state.streamProps,
+        configuration: {...this.state.streamProps.configuration, licenseKey: text}
       }
     })
   }
@@ -312,71 +246,19 @@ export default class App extends React.Component {
   onStreamNameChange (text) {
     this.setState({
       streamNameFieldProps: {...this.state.streamNameFieldProps, value: text},
-      videoProps: {...this.state.videoProps,
-        configuration: {...this.state.videoProps.configuration, streamName: text}
+      streamProps: {...this.state.streamProps,
+        configuration: {...this.state.streamProps.configuration, streamName: text}
       }
     })
   }
 
-  onMetaData (event) {
-    console.log(`onMetadata :: ${event.nativeEvent.metadata}`)
-  }
-
-  onConfigured (event) {
-    console.log(`onConfigured :: ${event.nativeEvent.key}`)
-    this.red5pro_video.setState({
-      configured: true
-    })
-    if (this.state.isPublisher) {
-      publish(findNodeHandle(this.red5pro_video), this.state.videoProps.configuration.streamName)
-    }
-    else {
-      subscribe(findNodeHandle(this.red5pro_video), this.state.videoProps.configuration.streamName)
-    }
-  }
-
-  onPublisherStreamStatus (event) {
-    console.log(`onPublisherStreamStatus :: ${JSON.stringify(event.nativeEvent.status, null, 2)}`)
-    const status = event.nativeEvent.status
-    let message = isValidStatusMessage(status.message) ? status.message : status.name
-    if (!this.state.inErrorState) {
-      this.setState({
-        toastProps: {...this.state.toastProps, value: message},
-        isInErrorState: (status.code === 2)
-      })
-    }
-  }
-
-  onSubscriberStreamStatus (event) {
-    console.log(`onSubscriberStreamStatus :: ${JSON.stringify(event.nativeEvent.status, null, 2)}`)
-    const status = event.nativeEvent.status
-    let message = isValidStatusMessage(status.message) ? status.message : status.name
-    if (!this.state.inErrorState) {
-      this.setState({
-        toastProps: {...this.state.toastProps, value: message},
-        isInErrorState: (status.code === 2)
-      })
-    }
-  }
-
-  onUnsubscribeNotification (event) {
-    console.log(`onUnsubscribeNotification:: ${JSON.stringify(event.nativeEvent.status, null, 2)}`)
+  onStop (event) {
+    console.log('App:onStop()')
     this.setState({
       hasStarted: false,
-      isInErrorState: false,
-      toastProps: {...this.state.toastProps, value: 'waiting...'}
+      isInErrorState: false
     })
   }
-
-  onUnpublishNotification (event) {
-    console.log(`onUnpublishNotification:: ${JSON.stringify(event.nativeEvent.status, null, 2)}`)
-    this.setState({
-      hasStarted: false,
-      isInErrorState: false,
-      toastProps: {...this.state.toastProps, value: 'waiting...'}
-    })
-  }
-
 }
 
 const styles = StyleSheet.create({
@@ -399,30 +281,5 @@ const styles = StyleSheet.create({
     right: 0,
     textAlign: 'center',
     padding: 20
-  },
-  videoView: {
-    flex: 1,
-    flexDirection: 'row',
-    // justifyContent: 'center',
-    backgroundColor: 'black'
-  },
-  buttonView: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    bottom: 0,
-    height: 40,
-    backgroundColor: 'blue',
-    color: 'white'
-  },
-  toast: {
-    color: 'white',
-    left: 0,
-    right: 0,
-    bottom: 0,
-    padding: 10,
-    textAlign: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 1.0)'
   }
 })
-
