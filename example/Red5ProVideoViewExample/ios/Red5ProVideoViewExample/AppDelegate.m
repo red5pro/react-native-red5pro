e382b5ae72d44095a59e5d06e6437f50e9497fd9
bustardcelly <bustardcelly@gmail.com>
2020-02-18 08:13:18 -0500

    fixing darkmode text issues in iOS example.

--- example/Red5ProVideoViewExample/ios/Red5ProVideoViewExample/AppDelegate.m
+++ example/Red5ProVideoViewExample/ios/Red5ProVideoViewExample/AppDelegate.m
@@ -21,6 +21,7 @@
                                             initialProperties:nil];
 
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  rootView.overrideUserInterfaceStyle = UIUserInterfaceStyleLight;
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
