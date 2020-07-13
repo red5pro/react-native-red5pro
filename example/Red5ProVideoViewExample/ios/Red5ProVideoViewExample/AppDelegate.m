a83e7ea84c80e885b5c408de1f8d94edc596325d
bustardcelly <bustardcelly@gmail.com>
2020-07-13 10:00:28 -0400

    enforcing style light for Android 13+

--- example/Red5ProVideoViewExample/ios/Red5ProVideoViewExample/AppDelegate.m
+++ example/Red5ProVideoViewExample/ios/Red5ProVideoViewExample/AppDelegate.m
@@ -21,7 +21,9 @@
                                             initialProperties:nil];
 
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-  rootView.overrideUserInterfaceStyle = UIUserInterfaceStyleLight;
+  if (@available(iOS 13.0, *)) {
+    rootView.overrideUserInterfaceStyle = UIUserInterfaceStyleLight;
+  }
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
@@ -34,7 +36,7 @@
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
 #if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index-modular.ios" fallbackResource:nil];
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
